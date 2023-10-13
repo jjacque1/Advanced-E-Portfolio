f0d4b2e3008f11cb05fbb786212da7f284461123
jjacque1 <jacksonjacque.jj@gmail.com>
2023-10-12 22:53:41 -0400

    modal__form js in progress

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-// template_cuckcrp
-// service_qn6wr1c
-// 9LHVBFFS8KSaWHz2h
-
-function contact() {
-    console.log("it worked")
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_qn6wr1c",
+      "template_cuckcrp",
+      event.target,
+      "9LHVBFFS8KSaWHz2h"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on jacksonjacque.jj@gmail.com"
+      );
+    });
 }
+
+
+// 2:08:45
+
