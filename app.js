49e8bc2d418b0640fdd0f62c9125041720b02bdf
jjacque1 <jacksonjacque.jj@gmail.com>
2023-10-13 20:07:30 -0400

    modal completed

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ function contact(event) {
     });
 }
 
-
-// 2:08:45
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
 
