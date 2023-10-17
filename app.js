840375d678d3b08ed5b796383aba74005cf42258
jjacque1 <jacksonjacque.jj@gmail.com>
2023-10-16 20:17:49 -0400

    shapes animation javascript completed

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-let contrastToggle = false;
+const isModalOpen = false;
+const contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+
+
+
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
@@ -35,7 +53,7 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
